cd1611954f22e97d7c3bd9a6469c1792d73498ad
Tom Tate <tomtate101@yahoo.com>
2025-05-15 12:25:39 -0700

    add working dot button and fix equal operator bug

--- calculator.js
+++ calculator.js
@@ -1,17 +1,19 @@
-let num1 = 0;
-let num2 = 0;
-let currentOperator = "";
+const DISPLAY_MAX_SIZE = 11;
+
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
 
 const clearButton = document.querySelector("#clear");
 const backButton = document.querySelector("#backspace");
 const signButton = document.querySelector("#sign");
 const display = document.querySelector(".display");
+const dotButton = document.querySelector("#dot");
 const digitButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
 
-
-// clearButton.addEventListener()
-clearButton.addEventListener("click", handleClear);
+clearButton.addEventListener("click", clearCalculator);
+dotButton.addEventListener("click", addDot)
 digitButtons.forEach(button => button.addEventListener("click", handleDigitClick));
 operatorButtons.forEach(button => button.addEventListener("click", handleOperatorClick));
 
@@ -26,65 +28,99 @@ const divide = (a, b) => {
     return a / b;
 }
 
-function operator(a, b, op) {
-    switch (op){
-        case "+": return add(a,b);
-        case "-": return subtract(a,b);
-        case "*": return multiply(a,b);
-        case "/": return divide(a,b);
-        default: return null;
+function operate(a, b, operator) {
+    if (!a)
+        a = 0;
+    
+    if (!b)
+        b = 0;
+
+    a = Number(a);
+    b = Number(b);
+    
+    let result = 0;
+    switch (operator){
+        case "+": 
+            result = add(a,b);
+            break;
+        case "-": 
+            result = subtract(a,b);
+            break;
+        case "*": 
+            result = multiply(a,b);
+            break;
+        case "/": 
+            result = divide(a,b);
+            break;
+        default: 
+            result = null;
     }
+    
+    return result;
 }
 
 function handleDigitClick(event){
-    let displayNum = Number(event.target.textContent);
-    if (currentOperator == "")
+    let digitClicked = event.target.textContent;
+    if (firstOperator && secondOperand == null)
     {
-        updateDisplay(displayNum);
-        num1 = Number(display.textContent);
+        setDisplay(digitClicked);
+        secondOperand = 0;
+        return;
     }
-    else if (num2 == 0){
-        display.textContent = 0;
-        updateDisplay(displayNum);
-        num2 = Number(display.textContent);
-    }
-    else{
-        updateDisplay(displayNum);
-        num2 = Number(display.textContent);
-    }
+    concatDisplay(digitClicked);
 
 }
 
 function handleOperatorClick(event){
-    let eventOp = event.target.textContent;
-    let displayNum = Number(display.textContent);
-    if (currentOperator == "")
+    let clickedOperator = event.target.textContent;
+    if (firstOperator == null && clickedOperator === "=")
+        return;
+
+    if (firstOperator == null)
     {
-        num1 = displayNum;
-        currentOperator = eventOp;
+        firstOperand = getDisplay();
+        firstOperator = clickedOperator;
+        return;
     }
-    else
-    {
-        num1 = operator(num1,num2,currentOperator);
-        display.textContent = num1;
-        num2 = 0;
-        if (eventOp == "=")
-            currentOperator = ""
-        else
-            currentOperator = eventOp;
-    }
+
+    secondOperand = getDisplay();
+    setDisplay(operate(firstOperand,secondOperand,firstOperator));
+    firstOperand = getDisplay();
+    secondOperand = null;
+
+    if (clickedOperator === "=")
+        firstOperator = null;
+    else 
+        firstOperator = clickedOperator;
 }
 
-function handleClear(event) {
-    display.textContent = 0;
-    num1 = 0;
-    num2 = 0;
-    currentOperator = "";
+function clearCalculator() {
+    setDisplay(0);
+    firstOperand = null;
+    secondOperand = null;
+    firstOperator = null;
 }
 
-function updateDisplay(num){
-    if (display.textContent == 0)
-        display.textContent = num;
+function concatDisplay(num){
+    if (getDisplay() == 0 && !getDisplay().includes("."))
+        setDisplay(num);
     else
         display.textContent += num;
+
+    display.textContent = getDisplay().slice(0,DISPLAY_MAX_SIZE);
+}
+
+function addDot(){
+    if (getDisplay().includes("."))
+        return;
+    display.textContent += ".";
+}
+
+function setDisplay(value) {
+    let valueStr = String(value);
+    display.textContent = valueStr.slice(0,DISPLAY_MAX_SIZE);
+}
+
+function getDisplay(){
+    return display.textContent;
 }
